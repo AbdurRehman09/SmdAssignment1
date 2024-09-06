52700ccee096c2ea8504296a24f09cfc16f9a88b
AbdurRehman09 <l216098@lhr.nu.edu.pk>
2024-09-06 17:23:25 +0500

    2 screens are added

---
+++ app/src/main/java/com/example/assignment1/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // Delay for 3 seconds (3000 milliseconds) and then start MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SenderFormActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+
+}
