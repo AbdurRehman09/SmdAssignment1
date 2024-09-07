5e5c9e6ba1a318ae8724f860d04ea6fd8b9a2abd
AbdurRehman09 <l216098@lhr.nu.edu.pk>
2024-09-07 14:15:39 +0500

    3rd screen is added

--- app/src/main/java/com/example/assignment1/Review.java
+++ app/src/main/java/com/example/assignment1/Review.java
@@ -1,42 +1,24 @@
 package com.example.assignment1;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SenderFormActivity extends AppCompatActivity {
-
-    Button next_button;
+public class Review extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sender_information);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_review);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-        init();
-        next_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-              Intent i=new Intent(SenderFormActivity.this,RecieverFormInformation.class);
-              startActivity(i);
-              finish();
-            }
-        });
-
-    }
-    private void init(){
-        next_button=findViewById(R.id.next_button);
     }
 }
-
